e746b6c16fcd440b25f4c8ba9b8f003dfb7ff1a5
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-08-15 22:56:34 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -main functiom
+ *
+ * Return: o on sucess
+ */
+
+int main(void)
+{
+	printf("size of a char: %lu byte(s)", sizeof(char));
+	printf("size of int: %lu byte(s)", sizeof(int))
+	printf("sie of a long int: %lu byte(s)", sizeof(long int));
+	printf("Szie of a float: %lu byte(s)", sizeof(float);
+	return (0);
