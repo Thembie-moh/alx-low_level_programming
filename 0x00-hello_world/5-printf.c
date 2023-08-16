3723c009ccae9cc1bbe171816ce143d0c8c336d0
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-08-15 22:18:41 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stadio.h>
+
+/**
+ * main - this is the main function
+ * 
+ * Return: 0 when successfull
+ * */
+
+int main(void)
+{
+	Printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
