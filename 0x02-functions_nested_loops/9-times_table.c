9349b00775377fe1985a2fe47b0675384f4fc396
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-08-24 05:59:48 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-
+#include <stdio.h>
 /**
  * times_table - prints the 9 times table, starting with 0
  */
@@ -24,10 +24,10 @@ void times_table(void)
 				_putchar(k + '0');
 			} else if (k >= 10)
 			{
-				_pucchar(',');
+				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar(k % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
