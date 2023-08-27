fcdc6ca8ca789aeb7bd45b934509875f27f086f5
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-08-26 20:18:19 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,13 @@
 #include "main.h"
  /**
- * _isupper - checks if character is lower or upper case
- *
+ * isupper - checks if character is lower or upper case
  * @c: declared variable text
  * Return: 1 if c is uppercase and 0 otherwise
  */
-
-int _isupper(int c)
+int isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
-
 		return (0);
 }
