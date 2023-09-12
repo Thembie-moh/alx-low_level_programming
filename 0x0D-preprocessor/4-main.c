6fc1bb6c62674ded3904b3b1c4ca41c2f964111a
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-09-11 23:30:30 -0700

    4-sum

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int numSum;
+
+	numSum = SUM(98, 1024);
+	printf("%d\n", numSum);
+	return (0);
+}
