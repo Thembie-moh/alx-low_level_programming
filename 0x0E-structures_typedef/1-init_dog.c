7ac803a87e192dc969ee39ea55aed5ff292eedc4
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-09-12 20:47:01 -0700

    1-init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - initializes my dog
+ * @d: pointer to my puppy
+ * @name: name of my puppy
+ * @age: age of my puppy
+ * @owner: name of puppy owner
+ */
+void dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+	d->dogName = dogName
+	d->dogAge = dogAge
+	d->dogOwner = dogOwner
+	}
+}
+
---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_puppy;
+
+	init_dog(&my_puppy, "Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_puppy.dogName, my_puppy.dogAge);
+	return (0);
+}
