44045bdb12a25fe1ac1c24d8b2e915893e0909ba
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-10-04 20:33:59 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,26 +9,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *concatenate;
+	char *str_concat;
 	int y, si;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		c2 = "";
+		s2 = "";
 	y = si = 0;
 
 while (s1[y] != '\0')
-
 {
-	concatenate[y] = s1[y];
+	str_concat[y] = s1[y];
 	y++;
 }
 while (s2[si] != '\0')
 {
-	concatenate[y] = s2[si];
+	str_concat[y] = s2[si];
 	y++, si++;
 }
-concatenate[y] = '\0';
-return (concatenate);
+str_concat[y] = '\0';
+return(str_concat);
 }
