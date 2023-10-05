ef6fca0577209ea3bcaf0f124cc5d7f7d3601383
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-10-04 18:29:56 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *string;
 	unsigned int y;
 
-	string = malloc(size(char) * size;
+	string = malloc(sizeof(char) * size);
 			if (size == 0 || string == NULL)
 			return (NULL);
 
