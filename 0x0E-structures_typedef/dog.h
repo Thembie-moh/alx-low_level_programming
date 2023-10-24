e38fc92f01ea26ad159e75bd8713dd9951c7a1aa
Thembie-moh <Thembsiemohlomi@gmail.com>
2023-10-24 00:28:40 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a function struct is dog
+ * @name: The puppys name
+ * @age: The dogs age 
+ * @owner: The owners name 
+ * Return: The dogs name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
