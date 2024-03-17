23c0f884e12e5110a7e9d1584bf208579e3049f6
Thembie-moh <Thembsiemohlomi@gmail.com>
2024-03-17 05:19:02 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /**
- * main - writes a program that prints a name
+ * main - writes a program that prints it own  name
  * @argc: This counts the arguements
- * @argv: The arguement vector
+ * @argv: This writes the actual argument passed to the terminal
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
