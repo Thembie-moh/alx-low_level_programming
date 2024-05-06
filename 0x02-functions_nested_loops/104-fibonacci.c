5cf5fc4e6b992eed27a26a62a94362f4a6d45014
Thembie_moh <Thembsiemohlomi@gmail.com>
2024-05-06 12:14:09 +0000

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,22 +2,22 @@
 
 /**
  * main - The function will print the 1st 98 nums of a fibb sequence
- * 1 and 2 will be the starting nums of this sequence 
- * Retrun: )
+ * 1 and 2 will be the starting nums of this sequence
+ * Return: 0
 */
 int main(void)
 
 {
-        int count;
-        unsigned long fibo1 = 0, fibo2 = 1, total;
-        unsigned long fibo1_div1, fibo1_div2, fibo2_div1, fibo2_div2;
+	int count;
+	unsigned long fibo1 = 0, fibo2 = 1, total;
+	unsigned long fibo1_div1, fibo1_div2, fibo2_div1, fibo2_div2;
 	unsigned long div1, div2;
 
 
-        for (count = 0; count < 92; count++)
-        {
-        total = fibo1 + fibo2;
-        printf("%lu," total);
+	for (count = 0; count < 92; count++)
+	{
+	total = fibo1 + fibo2;
+	printf("%lu," total);
 	fibo1 = fibo2;
 	fibo2 = total;
 	}
@@ -26,7 +26,7 @@ int main(void)
 	fibo1_div2 = fibo1 % 10000000000;
 	fibo2_div2 = fibo2 % 10000000000;
 
-	for(count =93; count < 99; count++)
+	for (count = 93; count < 99; count++)
 	{
 	div1 = fibo1_div1 + fibo2_div1;
 	div2 = fibo1_div2 + fib2_div2;
@@ -35,7 +35,7 @@ int main(void)
 	div1 += 1;
 	div2 %= 10000000000;
 	}
-	printf("%lu%lu" , div1, div2);
+	printf("%lu%lu", div1, div2);
 	if (count != 98)
 	printf(",");
 	fibo1_div1 = fibo2_div1;
@@ -45,5 +45,4 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
-}
 }
