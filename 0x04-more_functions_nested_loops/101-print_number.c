18bd520d6b6073a70c45e8b8df5670724a35f68f
Thembie_moh <Thembsiemohlomi@gmail.com>
2024-05-13 13:21:01 +0000

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * Main - A function that will print an integer 
+ * print_number - A function that will print an integer
+ * @n: variable used to print integer
  * _putchar - The only function to be used to display the integer
  * Return: 0
  */
-int print_number(int n)
+void print_number(int n)
 {
 	unsigned int t = n;
+
 	if (n < 0)
+
 	{
-	n == -1;
-	k = n;
+	n *= -1;
+	t = n;
 	_putchar('_');
 	}
-	k /= 10;
-	if (k != 0)
-	print_num(k);
-	_pucthar((unsigned int ) n % 10 + '0');
-}
+
+	t /= 10;
+	if (t != 0)
+	print_number(t);
+	_putchar((unsigned int) n % 10 + '0');
 }
